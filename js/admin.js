a3c9ddf56e9e6941b71cb63751f6c8731f181708
Benjamin Lack <benjaminshlack@gmail.com>
2015-11-17 19:56:52 +0000

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -10,8 +10,8 @@ $(function(){
 
     // Get data from the form and put them into variables
         var data = $(this).serializeArray(),
-            username = data[0].value,
-            password = data[1].value;
+            username = data[1].value,
+            password = data[2].value;
     
 
     // Call Parse Login function with those variables
